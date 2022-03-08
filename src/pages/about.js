b4ce8689e5fcdaa0f69c7a984c52495cbda2d1d3
uz <uz@icd.com>
2022-03-08 11:24:38 +0800

    add css modules

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,15 @@
 // 第1步：导入React
 import * as React from 'react'
-import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
-// 第2步：定义你的组件
+// 第2步：定义您的组件
 const AboutPage = () => {
     return (
-        <main>
-            <title>About Me</title>
-            <h1>About Me</h1>
-            <Link to="/">Back to Home</Link>
+        <Layout pageTitle="About Me">
             <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-        </main>
+        </Layout>
     )
 }
-// 第3步：导出你的组件
+
+// 第3步：导出组件
 export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,15 @@
 // 第1步：导入React
 import * as React from 'react'
-import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
 // 第2步：定义您的组件
 const IndexPage = () => {
-  return (
-      <main>
-        <title>Home Page</title>
-        <h1>Welcome to my Gatsby site!</h1>
-        <Link to="/about">About</Link>
-        <p>I'm making this by following the Gatsby Tutorial.</p>
-      </main>
-  )
+    return (
+        <Layout pageTitle="Home Page">
+            <p>I'm making this by following the Gatsby Tutorial.</p>
+        </Layout>
+    )
 }
+
 // 第3步：导出组件
 export default IndexPage
