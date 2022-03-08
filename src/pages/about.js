3afe1f7757ff89de8aed913d3cfba69234f4eabd
uz <uz@icd.com>
2022-03-08 10:27:49 +0800

    a

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,14 @@
 // 第1步：导入React
 import * as React from 'react'
+import { Link } from 'gatsby'
+
 // 第2步：定义你的组件
 const AboutPage = () => {
     return (
         <main>
             <title>About Me</title>
             <h1>About Me</h1>
+            <Link to="/">Back to Home</Link>
             <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
         </main>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 // 第1步：导入React
 import * as React from 'react'
+import { Link } from 'gatsby'
+
 // 第2步：定义您的组件
 const IndexPage = () => {
   return (
       <main>
         <title>Home Page</title>
         <h1>Welcome to my Gatsby site!</h1>
+        <Link to="/about">About</Link>
         <p>I'm making this by following the Gatsby Tutorial.</p>
       </main>
   )
